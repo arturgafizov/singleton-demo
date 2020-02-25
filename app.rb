3a3eba42c4ed245082a3bea54b0ebb58345743d9
Artur Gafizov <art05xxx@rambler.ru>
2020-02-25 07:43:04 +0300

    Update logger

--- app.rb
+++ app.rb
@@ -1,10 +1,22 @@
 class Logger
+	def initialize
+		@f = File.open 'log.txt', 'a'
+	end	
+	# class method   
 	def self.say_something
 		puts "haha"
+	end	
+
+	# instance method
+	def log_something wat
+		@f.puts wat
+#		@f.close
 	end	
 end	
 
 Logger.say_something
 
-#logger = Logger.new
-#logger.say_something
+logger = Logger.new
+logger.log_something 'hey!'
+#Logger.log_something 'wazzup'
+
