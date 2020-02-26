eebef9db6fc4df3470109f8a5b534d856ba46944
Artur Gafizov <art05xxx@rambler.ru>
2020-02-26 07:06:58 +0300

    Update logger to unclude self.instance

--- app.rb
+++ app.rb
@@ -2,6 +2,17 @@ class Logger
 	def initialize
 		@f = File.open 'log.txt', 'a'
 	end	
+
+	@@x = nil
+
+	def self.instance
+		if @@x == nil
+			@@x = Logger.new
+		end	
+		
+		return @@x	
+	end	
+
 	# class method   
 	def self.say_something
 		puts "haha"
@@ -15,6 +26,8 @@ class Logger
 end	
 
 Logger.say_something
+Logger.instance.log_something 'blabla'
+Logger.instance.log_something 'blabla2'
 
 logger = Logger.new
 logger.log_something 'hey!'
